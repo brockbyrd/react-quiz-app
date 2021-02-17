494144843b5ee42697a6d21a1c509d4952ec29be
brockbyrd <brockbyrdd@gmail.com>
2021-02-17 12:52:56 -0600

    creates checkAnswer function to check user answer

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,22 @@ const App = () => {
   };
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-
+    if (!gameOver) {
+      //user's answer
+      const answer = e.currentTarget.value;
+      //check answer against correct answer
+      const correct = questions[number].correct_answer === answer
+      //add score if correct
+      if (correct) setScore(prev => prev + 1)
+      //save answer in array for user answers
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers(prev => [...prev, answerObject]);
+    }
   };
 
   const nextQuestion = () => {
