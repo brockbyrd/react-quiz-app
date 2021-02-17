eb87d17adb07eef4b03bde94bde0af01b2217c49
brockbyrd <brockbyrdd@gmail.com>
2021-02-17 12:01:38 -0600

    creates functionality for startQuiz function

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
 import React, { useState } from 'react';
+//components
 import QuestionCard from './components/QuestionCard'
+//api call/types
+import { QuestionState, Difficulty, fetchQuizQuestions } from './API'
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
   const startQuiz = async () => {
+    setLoading(true);
+    setGameOver(false);
 
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.MEDIUM
+    )
+
+    setQuestions(newQuestions)
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false)
   };
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -31,14 +53,14 @@ const App = () => {
       </button>
       <p className="score">Score: </p>
       <p>Loading Questions...</p>
-      <QuestionCard
+      {/* <QuestionCard
       questionN={number +  1}
       totalQuestions={TOTAL_QUESTIONS}
       question={questions[number].question}
       answers={questions[number].answers}
       userAnswer={userAnswers ? userAnswers[number] : undefined}
       callback={checkAnswer}
-       />
+       /> */}
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
