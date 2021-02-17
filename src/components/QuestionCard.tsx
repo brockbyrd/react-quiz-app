be399cdb21dcc9bf6f69c2c4e46e5acba6dc2fe2
brockbyrd <brockbyrdd@gmail.com>
2021-02-17 11:21:28 -0600

    creates question card and passes in props

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,40 @@
 import React from 'react';
 
-const QuestionCard = () => <div>QuestionCard</div>
+type Props = {
+    question: string;
+    answers: string[];
+    callback: any;
+    userAnswer: any;
+    questionN: number;
+    totalQuestions: number;
+
+}
+
+const QuestionCard: React.FC<Props> = ({
+    question,
+    answers,
+    callback,
+    userAnswer,
+    questionN,
+    totalQuestions
+}) => (
+
+    <div>
+        <p className="number">
+            Question: { questionN } / { totalQuestions }
+        </p>
+        <p dangerouslySetInnerHTML={{ __html: question }} />
+        <div>
+            {answers.map(answer =>
+            <div>
+                <button disabled={ userAnswer } onClick={ callback }>
+                    <span dangerouslySetInnerHTML={{ __html: answer }}></span>
+                </button>
+            </div>
+                )}
+        </div>
+    </div>
+
+);
 
 export default QuestionCard;
