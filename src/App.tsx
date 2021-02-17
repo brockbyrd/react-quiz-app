846849ab1ac7e637924aa8c19ca1b1bf5917c384
brockbyrd <brockbyrdd@gmail.com>
2021-02-17 12:46:48 -0600

    creates ternaries to control what shows at the right time

--- src/App.tsx
+++ src/App.tsx
@@ -48,22 +48,30 @@ const App = () => {
   return (
     <div className="App">
       <h1>REACT QUIZ</h1>
-      <button className="start" onClick={startQuiz}>
-        Start Quiz
-      </button>
-      <p className="score">Score: </p>
-      <p>Loading Questions...</p>
-      {/* <QuestionCard
-      questionN={number +  1}
-      totalQuestions={TOTAL_QUESTIONS}
-      question={questions[number].question}
-      answers={questions[number].answers}
-      userAnswer={userAnswers ? userAnswers[number] : undefined}
-      callback={checkAnswer}
-       /> */}
-      <button className="next" onClick={nextQuestion}>
-        Next Question
-      </button>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ?
+        <button className="start" onClick={startQuiz}>
+          Start Quiz
+        </button> : null
+      }
+
+      {!gameOver ? <p className="score">Score: </p> : null}
+      {loading && <p>Loading Questions...</p>}
+      {!loading && !gameOver && (
+        <QuestionCard
+          questionN={number +  1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+      {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
+        <button className="next" onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
+
     </div>
   );
 }
